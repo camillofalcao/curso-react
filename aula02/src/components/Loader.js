f5f005f4aca7b8f952aeaee338d5979ea0eaf2a0
camillo-classroom <camillo.classroom@gmail.com>
2024-08-19 01:40:15 -0300

    State

---
+++ aula02/src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div class="loader-container">
+            <div class="loader"></div>
+        </div>
+    );
+};
+
+export default Loader;
--- aula02/src/components/Loja.js
+++ aula02/src/components/Loja.js
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Cabecalho from './Cabecalho';
 import Rodape from './Rodape';
 import LojaSkin from './LojaSkin';
+import Loader from './Loader';
 
 const Loja = () => {
+    const [estado, setEstado] = useState({ carregando: true, dados: [] });
+
+    useEffect(() => {
+        if (estado.carregando) {
+            setTimeout(() => {
+                setEstado({ carregando: false, dados: objetos });
+            }, 2000);
+        }
+    }, [estado.carregando]);
+
+    let componentes;
+
+    if (estado.carregando) {
+        componentes = <Loader />;
+    } else {
+        componentes = estado.dados.map(x => {
+            return (
+                <LojaSkin
+                    nome={x.nome}
+                    imagem={x.imagem}
+                    gemas={x.gemas}
+                />
+            );
+        })
+    }
+
     return (<>
         <Cabecalho />
         <main>
             <div className="loja">
-                <LojaSkin
-                    nome="Ariana Emberlym"
-                    imagem="a.png"
-                    gemas="2000"
-                />
-                <LojaSkin
-                    nome="Jasper Moonshadow"
-                    imagem="d.png"
-                    gemas="2000"
-                />
+                {
+                    componentes
+                }
             </div>
         </main>
         <Rodape />
     </>);
 };
+
+const objetos = [
+    { nome: 'Ariana Emberlym', imagem: 'a.png', gemas: 2000 },
+    { nome: 'Elowen Starcrest', imagem: 'b.png', gemas: 2000 },
+    { nome: 'Elias Nightshade', imagem: 'c.png', gemas: 2000 },
+    { nome: 'Jasper Moonshadow', imagem: 'd.png', gemas: 2000 },
+    { nome: 'Ronan Thornfield', imagem: 'e.png', gemas: 2000 },
+    { nome: 'Lila Blackwood', imagem: 'f.png', gemas: 2000 },
+    { nome: 'Mira Sunvale', imagem: 'g.png', gemas: 2000 },
+    { nome: 'Finnian Stormrider', imagem: 'h.png', gemas: 2000 },
+    { nome: 'Liam Ravenscroftn', imagem: 'i.png', gemas: 2000 }
+];
 
 export default Loja;
