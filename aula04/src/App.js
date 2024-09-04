05810480d7e4c8f46bf58be3af299c51c11d22d9
camillo-classroom <camillo.classroom@gmail.com>
2024-09-04 12:57:16 -0300

    Versão inicial aula04

---
+++ aula04/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Loja from './components/Loja';
+
+function App() {
+  return (
+    <div>
+      <Loja />
+    </div>
+  );
+}
+
+export default App;
---
+++ aula04/src/components/Loja.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import Cabecalho from './Cabecalho';
+import Rodape from './Rodape';
+import LojaSkin from './LojaSkin';
+import Loader from './Loader';
+
+const Loja = () => {
+    const [estado, setEstado] = useState({ carregando: true, dados: [] });
+
+    useEffect(() => {
+        setTimeout(() => {
+            setEstado({ carregando: false, dados: objetos });
+        }, 2000);
+    }, []);
+
+    let componentes;
+
+    if (estado.carregando) {
+        componentes = <Loader />;
+    } else {
+        componentes = estado.dados.map(x => {
+            return (
+                <LojaSkin
+                    nome={x.nome}
+                    imagem={x.imagem}
+                    gemas={x.gemas}
+                />
+            );
+        })
+    }
+
+    return (<>
+        <Cabecalho />
+        <main>
+            <div id="pesquisa">
+                <input type="text" />
+                <button>?</button>
+            </div>
+            <div className="loja">
+                {
+                    componentes
+                }
+            </div>
+        </main>
+        <Rodape />
+    </>);
+};
+
+const objetos = [
+    { nome: 'Ariana Emberlym', imagem: 'a.png', gemas: 2000 },
+    { nome: 'Elowen Starcrest', imagem: 'b.png', gemas: 2000 },
+    { nome: 'Elias Nightshade', imagem: 'c.png', gemas: 2000 },
+    { nome: 'Jasper Moonshadow', imagem: 'd.png', gemas: 2000 },
+    { nome: 'Ronan Thornfield', imagem: 'e.png', gemas: 2000 },
+    { nome: 'Lila Blackwood', imagem: 'f.png', gemas: 2000 },
+    { nome: 'Mira Sunvale', imagem: 'g.png', gemas: 2000 },
+    { nome: 'Finnian Stormrider', imagem: 'h.png', gemas: 2000 },
+    { nome: 'Liam Ravenscroftn', imagem: 'i.png', gemas: 2000 }
+];
+
+export default Loja;
